3affc64f70a4c54d98ce7b604b0ea89b27030f99
Diogopk <88211598+Diogopk@users.noreply.github.com>
2024-12-20 18:04:14 -0300

    BotATT.py

--- BotATT.py
+++ BotATT.py
@@ -61,7 +61,7 @@ def calculate_delay(hour, minute):
 
 # Função assíncrona para agendamento
 async def schedule_messages():
-    schedule_times = [(18, 00), (18, 01), (18, 02), (21, 0)]  # Horários para enviar as mensagens
+    schedule_times = [(18, 05), (18, 06), (18, 07), (21, 0)]  # Horários para enviar as mensagens
     while True:
         for hour, minute in schedule_times:
             delay = calculate_delay(hour, minute)
